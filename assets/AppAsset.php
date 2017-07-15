1afc5f1ef2691aac445621987a749839b67bdb78
Jony <jonneyless@163.com>
2017-07-15 17:17:32 +0800

    去掉 Jquery pin 控件

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -26,6 +26,5 @@ class AppAsset extends AssetBundle
         'ijony\admin\assets\PaceAsset',
         'ijony\admin\assets\MetisMenuAsset',
         'ijony\admin\assets\ToastrAsset',
-        'ijony\admin\assets\JQueryPinAsset',
     ];
 }
