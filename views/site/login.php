907f05dae8eed5520ca9272fa71e7dc3b2e3e099
Jony <jonneyless@163.com>
2017-08-10 14:20:47 +0800

    修复登录框

--- views/site/login.php
+++ views/site/login.php
@@ -11,19 +11,21 @@ $this->title = '登录';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="middle-box loginscreen">
-    <div>
+<?php $this->beginContent('@app/views/layouts/main-full.php');?>
+    <div class="middle-box loginscreen">
         <div>
-            <h1 class="logo-name">登录</h1>
+            <div>
+                <h1 class="logo-name">登录</h1>
+            </div>
+            <?php $form = ActiveForm::begin([
+                'id' => 'login-form',
+                'options' => ['class' => 'm-t'],
+            ]); ?>
+                <?= $form->field($model, 'username')->label(false)->textInput(['autofocus' => true, 'placeholder' => '账 号']) ?>
+                <?= $form->field($model, 'password')->label(false)->passwordInput(['placeholder' => '密 码']) ?>
+                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                <?= Html::submitButton('登录', ['class' => 'btn btn-primary block full-width m-b', 'name' => 'login-button']) ?>
+            <?php ActiveForm::end(); ?>
         </div>
-        <?php $form = ActiveForm::begin([
-            'id' => 'login-form',
-            'options' => ['class' => 'm-t'],
-        ]); ?>
-            <?= $form->field($model, 'username')->label(false)->textInput(['autofocus' => true, 'placeholder' => '账 号']) ?>
-            <?= $form->field($model, 'password')->label(false)->passwordInput(['placeholder' => '密 码']) ?>
-            <?= $form->field($model, 'rememberMe')->checkbox() ?>
-            <?= Html::submitButton('登录', ['class' => 'btn btn-primary block full-width m-b', 'name' => 'login-button']) ?>
-        <?php ActiveForm::end(); ?>
     </div>
-</div>
+<?php $this->endContent();?>
