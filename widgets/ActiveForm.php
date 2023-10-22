0da5145c9c6c5033ffd44d616f29a10ac5f82c3a
Jony <jonneyless@163.com>
2023-10-22 11:20:21 +0800

    修复表单

--- widgets/ActiveForm.php
+++ widgets/ActiveForm.php
@@ -4,8 +4,8 @@ namespace ijony\admin\widgets;
 
 use Yii;
 use yii\base\InvalidConfigException;
-use yii\bootstrap4\ActiveField;
 use yii\bootstrap4\Html;
+use yii\helpers\ArrayHelper;
 
 /**
  * A Bootstrap 3 enhanced version of [[\yii\widgets\ActiveForm]].
@@ -14,7 +14,7 @@ use yii\bootstrap4\Html;
  * So for example to render a horizontal form you would:
  *
  * ```php
- * use qooapp\admin\widgets\ActiveForm;
+ * use ijony\admin\widgets\ActiveForm;
  *
  * $form = ActiveForm::begin(['layout' => 'horizontal'])
  * ```
@@ -65,12 +65,13 @@ class ActiveForm extends \yii\bootstrap4\ActiveForm
      * @var string the default field class name when calling [[field()]] to create a new field.
      * @see fieldConfig
      */
-    public $fieldClass = 'ijony\admin\widgets\ActiveField';
+    public $fieldClass = ActiveField::class;
 
     /**
      * @var array HTML attributes for the form tag. Default is `[]`.
      */
     public $options = [];
+
     /**
      * @var string the form layout. Either 'default', 'horizontal' or 'inline'.
      * By choosing a layout, an appropriate default field configuration is applied. This will
@@ -78,32 +79,79 @@ class ActiveForm extends \yii\bootstrap4\ActiveForm
      * override these defaults through [[fieldConfig]].
      * @see \yii\bootstrap4\ActiveField for details on Bootstrap 3 field configuration
      */
-    public $layout = 'default';
+    public $layout = 'horizontal';
 
     /**
      * {@inheritdoc}
      */
     public function init()
     {
-        if (!in_array($this->layout, ['default', 'horizontal', 'inline'])) {
+        if (!in_array($this->layout, [self::LAYOUT_DEFAULT, self::LAYOUT_HORIZONTAL, self::LAYOUT_INLINE])) {
             throw new InvalidConfigException('Invalid layout type: ' . $this->layout);
         }
 
-        if ($this->layout !== 'default') {
+        if ($this->layout !== self::LAYOUT_DEFAULT) {
             Html::addCssClass($this->options, 'form-' . $this->layout);
         }
-        if (!isset($this->fieldConfig['class'])) {
-            $this->fieldConfig['class'] = 'ijony\admin\widgets\ActiveField';
+
+        if ($this->layout == self::LAYOUT_HORIZONTAL && !$this->fieldConfig) {
+            $this->fieldConfig = [
+                'inline' => true,
+                'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
+                'horizontalCssClasses' => [
+                    'label' => 'col-sm-2 col-form-label',
+                    'offset' => 'offset-sm-2',
+                    'wrapper' => 'col-sm-10',
+                    'error' => '',
+                    'hint' => '',
+                ],
+            ];
         }
+
         parent::init();
     }
 
     /**
      * {@inheritdoc}
-     * @return ActiveField the created ActiveField object
+     * @return \yii\bootstrap4\ActiveField|\ijony\admin\widgets\ActiveField
      */
-    public function field($model, $attribute, $options = []): ActiveField
+    public function field($model, $attribute, $options = [])
     {
+        $error = $model->getFirstError($attribute);
+        if ($error) {
+            Yii::$app->session->addFlash('error', $error);
+        }
+
+        $inputName = Html::getInputName($model, $attribute);
+        preg_match(Html::$attributeRegex, $attribute, $matches);
+        if (isset($options['suffix'])) {
+            $options['inputOptions']['id'] = Html::getInputId($model, $attribute) . '_' . rand(10000, 9999);
+            $options['inputOptions']['name'] = $inputName . $options['suffix'];
+            unset($options['suffix']);
+        }
+
+        if (is_string($options)) {
+            if ($this->layout == self::LAYOUT_DEFAULT) {
+                $this->fieldConfig['options']['class'] = 'form-group ' . $options;
+            }
+
+            $options = [];
+        }
+
+        if (isset($options['class'])) {
+            if ($this->layout == 'horizontal') {
+                $options['horizontalCssClasses'] = ArrayHelper::merge([
+                    'label' => 'col-sm-2 col-form-label',
+                    'offset' => 'offset-sm-2',
+                    'wrapper' => 'col-sm-10',
+                    'error' => '',
+                    'hint' => '',
+                ], $options['class']);
+            }
+
+            unset($options['class']);
+        }
+
         return parent::field($model, $attribute, $options);
     }
 }
